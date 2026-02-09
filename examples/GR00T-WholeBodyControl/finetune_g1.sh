39ff62a6e304173fb5a7d6263663dc5f7398ca06
rss-hiverobots <rss@hiverobots.dk>
2026-02-09 10:05:56 +0100

    adjust training for the 5090

--- examples/GR00T-WholeBodyControl/finetune_g1.sh
+++ examples/GR00T-WholeBodyControl/finetune_g1.sh
@@ -1,7 +1,8 @@
 
 set -x -e
 
-export NUM_GPUS=8
+export NUM_GPUS=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 torchrun --nproc_per_node=$NUM_GPUS --master_port=29500 \
     gr00t/experiment/launch_finetune.py \
@@ -12,10 +13,13 @@ torchrun --nproc_per_node=$NUM_GPUS --master_port=29500 \
     --output_dir /tmp/g1_finetune \
     --save_total_limit 5 \
     --max_steps 10000 \
+    --save_steps 2000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
-    --use_wandb \
-    --global_batch_size 1024 \
-    --dataloader_num_workers 6 \
-    --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08
+    --global_batch_size 16 \
+    --gradient_accumulation_steps 32 \
+    --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
+    --num-shards-per-epoch 100 \
+    --dataloader-num-workers 4 \
+    --shard-size 75 \
